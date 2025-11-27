9317f788c5a1ccde4a7d693b8b0b1c1f5a5fcb28
flatplum <flatplum@proton.me>
2025-11-28 01:32:07 +1100

    feat: add rudimentary header

--- app/components/AppHeader.vue
+++ app/components/AppHeader.vue
@@ -1,4 +1,33 @@
 <template>
-  <h1>This is the header!</h1>
-  <NuxtLink to="/committee">Committee!</NuxtLink>
+  <header class="site-nav">
+    <NuxtLink href="/">
+      <img class="site-nav__logo" src="https://lshtgdpdskhqqxdcwpjo.supabase.co/storage/v1/object/public/photos/SiiF-Logo-Backgroundless-Navy-2.png" alt="SiiF Logo">
+    </NuxtLink>
+    <ul class="site-nav__links">
+      <li class="text"><NuxtLink href="/committee">committee</NuxtLink></li>
+      <li class="text"><NuxtLink href="/publications">publications</NuxtLink></li>
+      <li class="text"><NuxtLink href="/events">events</NuxtLink></li>
+      <li class="text"><NuxtLink href="/contact">contact</NuxtLink></li>
+    </ul>
+  </header>
 </template>
+
+<style scoped>
+.site-nav {
+  height: 6rem;
+  background: var(--off-white);
+  padding: 0 6rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between
+}
+
+.site-nav__logo {
+  height: 4rem
+}
+
+.site-nav__links {
+  display: flex;
+  gap: 2rem
+}
+</style>
