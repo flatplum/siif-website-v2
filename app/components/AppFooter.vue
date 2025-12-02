7f555197208879d45e37de57b16170e96c5b5538
flatplum <flatplum@proton.me>
2025-12-02 22:20:36 +1100

    feat: add nojs popup

--- app/components/AppFooter.vue
+++ app/components/AppFooter.vue
@@ -1,3 +1,17 @@
 <template>
-  <h1>This is the footer!</h1>
+  <footer>
+    <noscript class="nojs-popup text--mono">
+      It looks like you don't have JavaScript enabled! The website should still work (though with a bit less functionality).
+    </noscript>
+  </footer>
 </template>
+
+<style scoped>
+.nojs-popup {
+  width: 100%;
+  display: block;
+  background-color: var(--off-black);
+  color: var(--off-white);
+  padding: 0.5rem;
+}
+</style>
