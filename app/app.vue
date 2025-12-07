18850962486549482fd543b1d6e917b9e280eb7a
flatplum <flatplum@proton.me>
2025-12-08 00:42:57 +1100

    fix: remove custom fonts from app.vue

--- app/app.vue
+++ app/app.vue
@@ -13,7 +13,7 @@
 @import url('~/assets/css/reusable.css');
 @import url('~/assets/css/global.css');
 
-@import url('~/assets/css/fonts/tex-gyre-heros.css');
+/* @import url('~/assets/css/fonts/tex-gyre-heros.css');
 @import url('~/assets/css/fonts/dm-serif-display.css');
-@import url('~/assets/css/fonts/firacode.css');
+@import url('~/assets/css/fonts/firacode.css'); */
 </style>
